1e60da4627c3adacc696fbe9f9af1495beb6ce0e
buoyantair <buoyantair@gmail.com>
2017-04-14 09:20:14 +0530

    a basic weather app with es6 & react

--- app/index.js
+++ app/index.js
@@ -1,15 +1,37 @@
-import Entity from './entity.js';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-class Hobbit extends Entity {
-  constructor(name, height){
-    super(name, height);
+const api_key = '0abf01162c513e88078456fc836a976d';
+
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      city: 'Mumbai',
+      description: ''
+    }
   }
 
-  greet(){
-    console.log(`Hello I am ${this.name} from the Shire!`)
+  componentDidMount(){
+    this.grabWeather(this.state.city);
+  }
+
+  grabWeather(city){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({description: json.weather[0].description})
+      })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1> Weather report for {this.state.city}!</h1>
+        <h2>{this.state.description}</h2>
+      </div>
+    )
   }
 }
 
-let frodo = new Hobbit("Frodo Baggins", 4.5);
-
-frodo.greet();
+ReactDOM.render(<App/>, document.getElementById("root"));
